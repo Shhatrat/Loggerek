cb5f71ae5fe635a73cc85eb507fb83c76473119c
Szymon Kaminski <szymonkaminski.ptb@gmail.com>
2025-01-14 15:14:44 +0100

    cleanup MainActivity

--- composeApp/src/androidMain/kotlin/com/shhatrat/loggerek/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/shhatrat/loggerek/MainActivity.kt
@@ -3,18 +3,8 @@ package com.shhatrat.loggerek
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import com.shhatrat.loggerek.base.color.LoggerekColor
-import com.shhatrat.loggerek.base.getTypography
 import org.koin.android.ext.koin.androidContext
 
 class MainActivity : ComponentActivity() {
@@ -24,21 +14,6 @@ class MainActivity : ComponentActivity() {
         setContent {
             App(calculateWindowSizeClass = { calculateWindowSizeClass(this) },
                 additionalKoinConfig = { this.androidContext(this@MainActivity) })
-        }
-    }
-}
-
-
-@Composable
-@Preview
-fun dd() {
-    MaterialTheme(
-        colors = LoggerekColor.lightColorScheme,
-        typography = getTypography()
-    ) {
-
-        Box(modifier = Modifier.fillMaxSize().background(Color.Red)) {
-        var text = "123"
         }
     }
 }
