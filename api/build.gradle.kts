5e085c7514ddb2e75b3b4f92a731415f56818b15
Szymon Kaminski <szymonkaminski.ptb@gmail.com>
2025-01-27 22:02:11 +0100

    enable compose resources in api module

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -8,6 +8,8 @@ plugins {
     alias(libs.plugins.androidLibrary)
     id("io.gitlab.arturbosch.detekt") version "1.23.7"
     kotlin("plugin.serialization") version "2.0.0"
+    alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.composeCompiler)
 }
 
 kotlin {
@@ -55,6 +57,7 @@ kotlin {
             implementation(libs.ktor.client.logging)
             implementation(libs.kotlinx.datetime)
             implementation(libs.koin.compose)
+            implementation(compose.components.resources)
             implementation("io.ktor:ktor-client-content-negotiation:3.0.0")
             implementation("io.ktor:ktor-serialization-kotlinx-json:3.0.0")
         }
