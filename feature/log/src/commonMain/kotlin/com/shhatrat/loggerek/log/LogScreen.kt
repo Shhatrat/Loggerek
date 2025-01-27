c5a8690831cd4db2b67dafd8716b6b06d27e4e0d
Szymon Kaminski <szymonkaminski.ptb@gmail.com>
2025-01-27 22:03:31 +0100

    implement log screen and viewmodel

--- feature/log/src/commonMain/kotlin/com/shhatrat/loggerek/log/LogScreen.kt
+++ feature/log/src/commonMain/kotlin/com/shhatrat/loggerek/log/LogScreen.kt
@@ -2,27 +2,58 @@ package com.shhatrat.loggerek.log
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.Crossfade
+import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.SnackbarHostState
+import androidx.compose.material.Text
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.shhatrat.loggerek.api.model.GeocacheMock
+import com.shhatrat.loggerek.base.LoggerekTheme
 import com.shhatrat.loggerek.base.WindowSizeCallback
 import com.shhatrat.loggerek.base.composable.CircularIndeterminateProgressBar
 import com.shhatrat.loggerek.base.composable.Header
+import com.shhatrat.loggerek.base.composable.MultiTextField
+import com.shhatrat.loggerek.base.composable.MultiTextFieldModel
 import com.shhatrat.loggerek.base.composable.SnackBarHelper
 import com.shhatrat.loggerek.base.composable.SnackBarHelper.ProvideSnackBar
-import kotlin.math.log
+import com.shhatrat.loggerek.base.composable.StarFiller
+import com.shhatrat.loggerek.base.composable.StarSwitch
+import com.shhatrat.loggerek.base.composable.VerticalSegmentedButton
+import com.shhatrat.loggerek.base.get
+import loggerek.feature.log.generated.resources.Res
+import loggerek.feature.log.generated.resources.cleanButton
+import loggerek.feature.log.generated.resources.logContent
+import loggerek.feature.log.generated.resources.notes
+import loggerek.feature.log.generated.resources.password
+import loggerek.feature.log.generated.resources.rate1
+import loggerek.feature.log.generated.resources.rate2
+import loggerek.feature.log.generated.resources.rate3
+import loggerek.feature.log.generated.resources.rate4
+import loggerek.feature.log.generated.resources.rate5
+import loggerek.feature.log.generated.resources.reccomend
+import loggerek.feature.log.generated.resources.sendButton
 
 @Composable
 fun LogScreen(calculateWindowSizeClass: WindowSizeCallback, logUiState: LogUiState) {
@@ -36,7 +67,6 @@ fun LogScreen(calculateWindowSizeClass: WindowSizeCallback, logUiState: LogUiSta
         when (screenClass) {
             WindowWidthSizeClass.Compact, WindowWidthSizeClass.Medium ->
                 CompactScreenLayout(modifier = Modifier.fillMaxSize(), logUiState)
-
             WindowWidthSizeClass.Expanded -> ExpandedScreenLayout(
                 Modifier.fillMaxSize(),
                 logUiState
@@ -53,7 +83,7 @@ fun ExpandedScreenLayout(modifier: Modifier, logUiState: LogUiState) {
 
 @Composable
 fun CompactScreenLayout(modifier: Modifier, settingsUiState: LogUiState) {
-    Box(modifier = modifier.background(MaterialTheme.colors.background).padding(16.dp)) {
+    Box(modifier = modifier.fillMaxSize().background(MaterialTheme.colors.background).padding(16.dp)) {
         AnimatedVisibility(settingsUiState.loader.active) {
             CircularIndeterminateProgressBar(
                 modifier = Modifier.align(Alignment.Center),
@@ -61,11 +91,122 @@ fun CompactScreenLayout(modifier: Modifier, settingsUiState: LogUiState) {
             )
         }
         Column(
-            Modifier.align(Alignment.TopCenter),
+            Modifier.fillMaxWidth().align(Alignment.TopCenter).verticalScroll(rememberScrollState()),
             verticalArrangement = Arrangement.spacedBy(16.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Header(Modifier, "LOG :) ")
+            settingsUiState.geocacheData?.let {
+                Header(Modifier, it.title)
+                Header(Modifier, it.titleLink)
+                VerticalSegmentedButton(
+                    it.logTypeData.types.map { it.get() },
+                    it.logTypeData.selectedIndex,
+                    it.logTypeData.onChangedIndex
+                )
+                it.ratingData?.let { rating ->
+                    var currentRating: Int? by remember { mutableStateOf(null) }
+                    if(rating.showRating) {
+                        StarFiller(currentSelectedIndex = currentRating, size = 5) {
+                            currentRating = it
+                            rating.starsOnChanged(it)
+                        }
+                    }
+                    Text(when(currentRating){
+                        0 -> Res.string.rate1
+                        1 -> Res.string.rate2
+                        2 -> Res.string.rate3
+                        3 -> Res.string.rate4
+                        null -> null
+                        else -> Res.string.rate5
+                    }?.get()?:"")
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        if(rating.recommendationPossible && rating.showRating) {
+                            Text(Res.string.reccomend.get())
+                            StarSwitch(selected = rating.recommendation, onClicked = {
+                                rating.recommendationChanged(!rating.recommendation)
+                            })
+                        }
+                    }
+                }
+                Text(Res.string.logContent.get())
+                MultiTextField(
+                    multiTextFieldModel = it.description,
+                    placeholder = { }
+                )
+                Text(Res.string.notes.get())
+                MultiTextField(
+                    multiTextFieldModel = it.myNotes,
+                    placeholder = { }
+                )
+
+                Text(Res.string.password.get())
+                it.password?.let { it1 ->
+                    MultiTextField(
+                        multiTextFieldModel = it1,
+                        lines = 2
+                    )
+                }
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Spacer(modifier = Modifier.fillMaxWidth().weight(1f))
+                    Button(onClick = { it.sendAction() }) {
+                        Text(Res.string.sendButton.get())
+                    }
+                    Spacer(modifier = Modifier.fillMaxWidth().weight(1f))
+                    Button(onClick = { it.resetAction() }) {
+                        Text(Res.string.cleanButton.get())
+                    }
+                    Spacer(modifier = Modifier.fillMaxWidth().weight(1f))
+                }
+            }
         }
+    }
+}
+
+@Composable
+fun SimpleScrollableList(items: List<String>) {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        items(items.size) { index ->
+            Text(
+                text = items[index],
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+                    .background(MaterialTheme.colors.primary.copy(alpha = 0.1f))
+                    .padding(16.dp),
+                style = MaterialTheme.typography.body1
+            )
+        }
+    }
+}
+
+@Preview()
+@Composable
+fun SimpleScrollableListPreview() {
+    val sampleItems = List(20) { "Item #${it + 1}" }
+    SimpleScrollableList(items = sampleItems)
+}
+
+@Preview
+@Composable
+fun LogScreenPreview() {
+    LoggerekTheme {
+        val gd = GeocacheMock().let { cache ->
+            GeocacheData(
+                title = cache.name,
+                titleLink = cache.url,
+                ratingData = RatingData(true, 5, {}, true, true, {}),
+                logTypeData = LogTypeData(0, cache.type.logType.logTypes.map { it.textRes }, onChangedIndex = {} ),
+                description = MultiTextFieldModel(),
+                myNotes = MultiTextFieldModel(),
+                password = MultiTextFieldModel(),
+                sendAction = {},
+                resetAction = {},
+            )
+        }
+        CompactScreenLayout(Modifier, LogUiState(geocacheData = gd))
     }
 }
--- feature/log/src/commonMain/kotlin/com/shhatrat/loggerek/log/LogViewModel.kt
+++ feature/log/src/commonMain/kotlin/com/shhatrat/loggerek/log/LogViewModel.kt
@@ -1,21 +1,56 @@
 package com.shhatrat.loggerek.log
 
-import com.shhatrat.loggerek.account.AccountManager
+import androidx.lifecycle.viewModelScope
+import com.shhatrat.loggerek.api.model.LogType
 import com.shhatrat.loggerek.base.BaseViewModel
+import com.shhatrat.loggerek.base.ButtonAction
 import com.shhatrat.loggerek.base.Error
 import com.shhatrat.loggerek.base.Loader
-import com.shhatrat.loggerek.base.MoveToIntro
+import com.shhatrat.loggerek.base.composable.MultiTextFieldModel
 import com.shhatrat.loggerek.base.error.ErrorHandlingUtil
 import com.shhatrat.loggerek.base.loader.LoaderHandlingUtil
+import com.shhatrat.loggerek.manager.log.LogManager
+import kotlinx.coroutines.launch
+import org.jetbrains.compose.resources.StringResource
 
 data class LogUiState(
     val loader: Loader = Loader(),
     val error: Error? = null,
+
+    val geocacheData: GeocacheData? = null
+)
+
+data class GeocacheData(
+    val title: String,
+    val titleLink: String,
+    val ratingData: RatingData? = null,
+    val logTypeData: LogTypeData,
+    val description: MultiTextFieldModel,
+    val myNotes: MultiTextFieldModel,
+    val password: MultiTextFieldModel?,
+    val sendAction: ButtonAction,
+    val resetAction: ButtonAction,
+)
+
+data class RatingData(
+    val showRating: Boolean = false,
+    val rating: Int? = null,
+    val starsOnChanged: (Int) -> Unit,
+    val recommendation: Boolean = false,
+    val recommendationPossible: Boolean,
+    val recommendationChanged: (Boolean) -> Unit,
+)
+
+data class LogTypeData(
+    val selectedIndex: Int,
+    val types: List<StringResource>,
+    val onChangedIndex: (Int) -> Unit
 )
 
 
 class LogViewModel(
-    private val cacheId: String
+    private val cacheId: String,
+    private val logManager: LogManager
 ) : BaseViewModel<LogUiState>(LogUiState()) {
 
     private val loaderHandlingUtil =
@@ -24,7 +59,103 @@ class LogViewModel(
     private val errorHandlingUtil =
         ErrorHandlingUtil { error -> updateUiState { copy(error = error) } }
 
+    private var logTypeData: FilledData = FilledData()
+
+    data class FilledData(
+//        val logType: LogType? = null,
+        val selectedType: LogType? = null,
+        val recommended: Boolean = false,
+    )
+
+    private val defaultLogTypeIndex = 0
+
     override fun onStart() {
         super.onStart()
+        viewModelScope.launch {
+            val cache = logManager.getCache(cacheId)
+            logTypeData = logTypeData.copy(selectedType = cache.type.logType.logTypes.first())
+            updateUiState {
+                copy(
+                    geocacheData = GeocacheData(
+                        title = cache.name,
+                        titleLink = cache.url,
+                        ratingData = RatingData(
+                            showRating = cache.type.logType.logTypes[defaultLogTypeIndex].canRate,
+                            starsOnChanged = {
+                                updateUiState { copy(geocacheData = geocacheData?.copy(ratingData = geocacheData.ratingData?.copy(rating = it))) }
+                            },
+                            recommendationPossible = logTypeData.selectedType?.canBeRecommended
+                                ?: false,
+                            recommendationChanged = {
+                                logTypeData = logTypeData.copy(recommended = it)
+                            }),
+                        logTypeData = LogTypeData(
+                            selectedIndex = defaultLogTypeIndex,
+                            types = cache.type.logType.logTypes.map { it.textRes },
+                            onChangedIndex = { changed ->
+                                updateUiState {
+                                    copy(
+                                        geocacheData = geocacheData?.copy(
+                                            ratingData = geocacheData.ratingData?.copy(
+                                                showRating = cache.type.logType.logTypes[changed].canRate,
+                                                ),
+                                            logTypeData = geocacheData.logTypeData.copy(
+                                                selectedIndex = changed
+                                            )
+                                        )
+                                    )
+                                }
+                            }),
+                        description = MultiTextFieldModel { changedText ->
+                            updateUiState {
+                                copy(
+                                    geocacheData = geocacheData?.copy(
+                                        description = geocacheData.description.copy(text = changedText)
+                                    )
+                                )
+                            }
+                        },
+                        myNotes = MultiTextFieldModel { changedText ->
+                            updateUiState {
+                                copy(
+                                    geocacheData = geocacheData?.copy(
+                                        myNotes = geocacheData.myNotes.copy(text = changedText)
+                                    )
+                                )
+                            }
+                        },
+                        password = MultiTextFieldModel { changedText ->
+                            updateUiState {
+                                copy(
+                                    geocacheData = geocacheData?.password?.copy(text = changedText)
+                                        ?.let {
+                                            geocacheData.copy(
+                                                password = it
+                                            )
+                                        }
+                                )
+                            }
+                        },
+                        sendAction = {},
+                        resetAction = {
+                            resetData()
+                        },
+                    )
+                )
+            }
+        }
+    }
+
+    private fun resetData() {
+        logTypeData = FilledData()
+        updateUiState {
+            copy(
+                geocacheData = geocacheData?.copy(
+                    description = geocacheData.description.copy(""),
+                    myNotes = geocacheData.myNotes.copy(""),
+                    password = geocacheData.password?.copy("")
+                )
+            )
+        }
     }
 }
