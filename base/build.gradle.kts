71a8b3f26a6f71a3e0bcfccc4443d925224460fb
Szymon Kaminski <szymonkaminski.ptb@gmail.com>
2025-04-18 20:58:30 +0200

    fix base gradle file

--- base/build.gradle.kts
+++ base/build.gradle.kts
@@ -9,39 +9,11 @@ plugins {
     id("io.gitlab.arturbosch.detekt") version "1.23.7"
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
+    id("android-config-plugin")
+    id("loggerek-plugin")
 }
 
 kotlin {
-    androidTarget {
-        @OptIn(ExperimentalKotlinGradlePluginApi::class)
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_11)
-        }
-    }
-
-    iosX64()
-    iosArm64()
-    iosSimulatorArm64()
-
-    jvm()
-
-    @OptIn(ExperimentalWasmDsl::class)
-    wasmJs {
-        browser {
-            val rootDirPath = project.rootDir.path
-            val projectDirPath = project.projectDir.path
-            commonWebpackConfig {
-                devServer = (devServer ?: KotlinWebpackConfig.DevServer()).apply {
-                    static = (static ?: mutableListOf()).apply {
-                        // Serve sources to debug inside browser
-                        add(rootDirPath)
-                        add(projectDirPath)
-                    }
-                }
-            }
-        }
-    }
-
     sourceSets {
         commonMain.dependencies {
             implementation(libs.koin.compose)
@@ -56,19 +28,11 @@ kotlin {
             implementation(projects.base.typeface)
             implementation(compose.components.resources)
             api(libs.androidx.lifecycle.viewmodel)
-            api("org.jetbrains.compose.material3:material3-window-size-class:1.7.3")
+            api(libs.material3.window.size.class1)
         }
     }
 }
 
 android {
     namespace = "com.shhatrat.loggerek.base"
-    compileSdk = libs.versions.android.compileSdk.get().toInt()
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    defaultConfig {
-        minSdk = libs.versions.android.minSdk.get().toInt()
-    }
 }
