ed1d1a7c9584f7e084aa3e4885a9003e35424789
Szymon Kaminski <szymonkaminski.ptb@gmail.com>
2025-05-01 21:36:35 +0200

    fix base modules

--- base/browser/build.gradle.kts
+++ base/browser/build.gradle.kts
@@ -13,7 +13,7 @@ kotlin {
         commonMain.dependencies {
             implementation(projects.base)
             implementation(projects.di)
-            implementation("org.jetbrains.compose.material3:material3-window-size-class:1.7.3")
+            implementation(libs.material3.window.size.class1)
         }
     }
 }
