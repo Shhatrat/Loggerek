ac778e7e6df27586616ebd1851d1b9b6756046e4
Szymon Kaminski <szymonkaminski.ptb@gmail.com>
2025-05-03 19:14:06 +0200

    fix profile module

--- feature/profile/build.gradle.kts
+++ feature/profile/build.gradle.kts
@@ -8,9 +8,8 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
     id("io.gitlab.arturbosch.detekt") version "1.23.7"
-    alias(libs.plugins.composeMultiplatform)
-    alias(libs.plugins.composeCompiler)
     id("io.github.takahirom.roborazzi")
+    id("feature-plugin")
 }
 
 roborazzi {
@@ -18,118 +17,20 @@ roborazzi {
 }
 
 kotlin {
-    androidTarget {
-        @OptIn(ExperimentalKotlinGradlePluginApi::class)
-        instrumentedTestVariant.sourceSetTree.set(KotlinSourceSetTree.test)
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_11)
-        }
-
-    }
-
-    iosX64()
-    iosArm64()
-    iosSimulatorArm64()
-
-    jvm()
-
-    @OptIn(ExperimentalWasmDsl::class)
-    wasmJs {
-        browser {
-            val rootDirPath = project.rootDir.path
-            val projectDirPath = project.projectDir.path
-            commonWebpackConfig {
-                devServer = (devServer ?: KotlinWebpackConfig.DevServer()).apply {
-                    static = (static ?: mutableListOf()).apply {
-                        // Serve sources to debug inside browser
-                        add(rootDirPath)
-                        add(projectDirPath)
-                    }
-                }
-            }
-        }
-    }
-
     sourceSets {
         commonMain.dependencies {
-            implementation(libs.koin.compose)
-            implementation(compose.runtime)
-            implementation(compose.foundation)
-            implementation(compose.material)
-            implementation(compose.ui)
-            implementation(compose.components.resources)
             implementation(projects.base)
             implementation(projects.base.color)
             implementation(projects.base.testing)
             implementation(projects.api)
             implementation(projects.manager.account)
-            implementation("org.jetbrains.compose.material3:material3-window-size-class:1.7.3")
-            @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
-            implementation(compose.uiTest)
-        }
-        androidUnitTest.dependencies {
-            implementation(libs.robolectric)
-            implementation(libs.espresso.core)
-            implementation(libs.ui.test.junit4)
-            implementation("androidx.compose.ui:ui-test-junit4-android:1.7.6")
-            implementation("androidx.compose.ui:ui-test-manifest:1.7.6")
-            implementation("androidx.compose.ui:ui-test-junit4-android")
-            implementation("io.github.takahirom.roborazzi:roborazzi-compose:1.39.0")
-            implementation("io.github.takahirom.roborazzi:roborazzi-junit-rule:1.39.0")
-            implementation("io.github.takahirom.roborazzi:roborazzi:1.39.0")
-
-        }
-        commonTest.dependencies {
-            implementation(kotlin("test"))
-        }
-        androidInstrumentedTest.dependencies {
-            implementation("io.github.takahirom.roborazzi:roborazzi-compose:1.39.0")
-            implementation("io.github.takahirom.roborazzi:roborazzi-junit-rule:1.39.0")
-            implementation("io.github.takahirom.roborazzi:roborazzi:1.39.0")
         }
         jvmTest.dependencies {
             implementation(compose.desktop.currentOs)
-            implementation("io.github.takahirom.roborazzi:roborazzi-compose-desktop:1.39.0")
-            implementation(kotlin("test"))
-        }
-        iosTest.dependencies {
-            implementation("io.github.takahirom.roborazzi:roborazzi-compose-ios:1.39.0")
-            implementation(kotlin("test"))
-        }
-        commonTest.dependencies {
-            implementation(kotlin("test")) // Wspólny framework do testów
-            implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
-            @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
-            implementation(compose.uiTest)
         }
     }
 }
 
 android {
     namespace = "com.shhatrat.loggerek.profile"
-    compileSdk = libs.versions.android.compileSdk.get().toInt()
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-
-    testOptions {
-        unitTests {
-            isIncludeAndroidResources = true
-        }
-    }
-
-    defaultConfig {
-        minSdk = libs.versions.android.minSdk.get().toInt()
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-}
-
-dependencies {
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4-android:1.7.6")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.7.6")
-    implementation(libs.roborazzi)
-    api(libs.roborazziRule)
-    api(libs.roborazziCompose)
-
 }
