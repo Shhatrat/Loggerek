9ffdedeaee10ac82299a138fa4abed3f34fc700e
Szymon Kaminski <szymonkaminski.ptb@gmail.com>
2025-05-06 11:21:35 +0200

    refactor LogManagerImpl and LogManager

--- manager/log/src/commonMain/kotlin/com/shhatrat/loggerek/manager/log/LogManager.kt
+++ manager/log/src/commonMain/kotlin/com/shhatrat/loggerek/manager/log/LogManager.kt
@@ -2,6 +2,7 @@ package com.shhatrat.loggerek.manager.log
 
 import com.shhatrat.loggerek.api.model.Geocache
 import com.shhatrat.loggerek.api.model.LogResponse
+import com.shhatrat.loggerek.api.model.LogType
 import com.shhatrat.loggerek.api.model.SubmitLogData
 
 
@@ -11,7 +12,7 @@ interface LogManager {
 
     suspend fun saveNote(id: String, note: String, oldValue: String)
 
-    suspend fun logCapabilities(id: String)
+    suspend fun logCapabilities(id: String): List<LogType>
 
     suspend fun submitLog(logData: SubmitLogData): LogResponse
 
